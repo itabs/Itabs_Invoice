c477c79cd5fcd0fb52f4c10fb7518398f9fd4301
Rouven Alexander Rieker <therouv@googlemail.com>
2013-04-11 10:41:37 +0200

    [+FEATURE] Implemented validation model

--- src/app/code/community/Itabs/Invoice/Model/Observer.php
+++ src/app/code/community/Itabs/Invoice/Model/Observer.php
@@ -25,10 +25,7 @@
  */
 class Itabs_Invoice_Model_Observer
 {
-    /**
-     * @var null|Mage_Sales_Model_Resource_Order_Collection
-     */
-    protected $_customerOrders = null;
+
 
     /**
      * paymentMethodIsActive
@@ -55,94 +52,8 @@ class Itabs_Invoice_Model_Observer
             return;
         }
 
-        // Get preconditions for checks
-        $customerGroupId = Mage_Customer_Model_Group::NOT_LOGGED_IN_ID;
-        if (Mage::app()->getStore()->isAdmin()) {
-            /* @var $session Mage_Adminhtml_Model_Session_Quote */
-            $session = Mage::getSingleton('adminhtml/session_quote');
-            /* @var $customer Mage_Customer_Model_Customer */
-            $customer = $session->getCustomer();
-            $customerGroupId = $session->getQuote()->getCustomerGroupId();
-        } else {
-            /* @var $session Mage_Customer_Model_Session */
-            $session = Mage::getSingleton('customer/session');
-            /* @var $customer Mage_Customer_Model_Customer */
-            $customer = $session->getCustomer();
-            if ($session->isLoggedIn()) {
-                $customerGroupId = $session->getCustomerGroupId();
-            }
-        }
-
-        // Check if payment is allowed only for specific customer groups
-        if (!Mage::getStoreConfigFlag('payment/invoice/specificgroup_all')) {
-            $allowedGroupIds = explode(',', Mage::getStoreConfig('payment/invoice/specificgroup'));
-            if (!in_array($customerGroupId, $allowedGroupIds)) {
-                $observer->getEvent()->getResult()->isAvailable = false;
-                return;
-            }
-        }
-
-        // Check minimum orders count
-        $minOrderCount = Mage::getStoreConfig('payment/invoice/customer_order_count');
-        if ($minOrderCount > 0) {
-            $customerId = $customer->getId();
-            if (is_null($customerId)) {
-                $observer->getEvent()->getResult()->isAvailable = false;
-                return;
-            }
-
-            $orders = $this->_getCustomerOrders($customerId);
-            if (count($orders) < $minOrderCount) {
-                $observer->getEvent()->getResult()->isAvailable = false;
-                return;
-            }
-        }
-
-        // Check minimum order amount
-        $minOrderSum = Mage::getStoreConfig('payment/invoice/customer_order_amount');
-        if ($minOrderSum > 0) {
-            $customerId = $session->getCustomerId();
-            if (is_null($customerId)) {
-                $observer->getEvent()->getResult()->isAvailable = false;
-                return;
-            }
-
-            $orders = $this->_getCustomerOrders($customerId);
-            $orderTotal = 0;
-            foreach ($orders as $order) {
-                $orderTotal += $order->getData('grand_total');
-            }
-
-            if ($orderTotal < $minOrderSum) {
-                $observer->getEvent()->getResult()->isAvailable = false;
-                return;
-            }
-        }
-    }
-
-    /**
-     * Retrieve the order collection of a specific customer
-     *
-     * @param  int                                        $customerId
-     * @return Mage_Sales_Model_Resource_Order_Collection
-     */
-    protected function _getCustomerOrders($customerId)
-    {
-        if (is_null($this->_customerOrders)) {
-            $orders = Mage::getResourceModel('sales/order_collection')
-                ->addAttributeToSelect('*')
-                ->addAttributeToFilter('customer_id', $customerId)
-                ->addAttributeToFilter('status', Mage_Sales_Model_Order::STATE_COMPLETE)
-                ->addAttributeToFilter(
-                    'state',
-                    array(
-                        'in' => Mage::getSingleton('sales/order_config')->getVisibleOnFrontStates()
-                    )
-                )
-                ->load();
-            $this->_customerOrders = $orders;
-        }
-
-        return $this->_customerOrders;
+        /* @var $validationModel Itabs_Invoice_Model_Validation */
+        $validationModel = Mage::getModel('invoice/validation');
+        $observer->getEvent()->getResult()->isAvailable = $validationModel->isValid();
     }
 }
